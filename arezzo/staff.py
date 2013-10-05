c8620257e9c8a7af34fd0bfab30ea25e285bd9a5
Lee Avital <leeavital@gmail.com>
2013-10-05 16:16:30 -0400

    Added instrument parameter in staff
    GMCode and instrument methods return GM code (general midi code)

--- arezzo/staff.py
+++ arezzo/staff.py
@@ -1,11 +1,20 @@
 class Staff():
    
-   def __init__( self ):
+   def __init__( self, instrument ):
 	  self.measures = []
+	  self.instrument = instrument
 
 
    def addMeasure( self, measure ):
 	  self.measures.append( measure ) 
+
+   
+   # yarg theses are the same
+   def GMCode( self ):
+	  return self.instrument
+
+   def instrument( self ):
+	  return self.instrument
 
    
    

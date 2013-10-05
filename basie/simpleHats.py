f1e9048d8d954b2ab4744f4b3746f449be0f5ee8
Lee Avital <leeavital@gmail.com>
2013-10-05 19:39:07 -0400

    Improvements to simpleHats.py
    Add comments in simpleHats.py
    Added bass drum in simpleHats.py
    (They're not so simple anymore!!!)

--- basie/simpleHats.py
+++ basie/simpleHats.py
@@ -2,16 +2,47 @@
 import arezzo
 
 
+# Drum to Note values (for reference only, this should be in an enum)
+# A#2 -- open high hat
+# F#2 -- closed high hat
+# B1 -- Acoustic Bass drum
+# C1 -- Bass drup
+# E2 --snare drum 2
+
+
 def simpleHats():
    
    m = arezzo.Measure()
    
-   m.addNote( arezzo.Note( 'A#2', 1, 4 ) )
-   m.addNote( arezzo.Note( 'F#2', 2, 12 ) )
-   m.addNote( arezzo.Note( 'F#2', 1, 12 ) )
+   
+   v = arezzo.Voice( )  
+   v.addNote( arezzo.Note( 'A#2', 1, 4) )  
+   v.addNote( arezzo.Note( 'F#2', 2, 12) )
+   v.addNote( arezzo.Note( 'F#2', 1, 12 ) )
 
-   m.addNote( arezzo.Note( 'A#2', 1, 4 ) )
-   m.addNote( arezzo.Note( 'F#2', 2, 12 ) )
-   m.addNote( arezzo.Note( 'F#2', 1, 12 ) )
+   v.addNote( arezzo.Note( 'A#2', 1, 4 ) )
+   v.addNote( arezzo.Note( 'F#2', 2, 12 ) )
+   v.addNote( arezzo.Note( 'F#2', 1, 12 ) )	 
+
+   m.addVoice( v )
+   
+   # bass drum
+   v = arezzo.Voice()
+   v.addNote( arezzo.Note( 'B1', 8, 12)  )
+   v.addNote( arezzo.Note( 'B1', 3, 12 )  )
+   v.addNote( arezzo.Note( 'B1', 1, 12 )  )
+
+   m.addVoice( v )
+
+   
+   # # bass drum
+   # v = arezzo.Voice()
+   # v.addNote( arezzo.Note( 'C2', 8, 12)  )
+   # v.addNote( arezzo.Note( 'C2', 3, 12)  )
+   # v.addNote( arezzo.Note( 'C2', 1, 12)  )
+
+   m.addVoice( v )
+
+
 
    return m

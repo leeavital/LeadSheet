5da26808a44ebe07f1ace9419f37326e20c94f05
Lee Avital <leeavital@gmail.com>
2013-10-06 03:37:20 -0400

    New build system outputs and plays mp3

--- build_and_play.sh
+++ build_and_play.sh
@@ -1,3 +1,5 @@
 # runs AL mid and plays the file with timidity 
 python al_mid.py infile=examples/autumn_leaves.txt tempo=200 repeat=2
-timidity output.mid -D 11 ## wtf, should be 11
+## wtf, not really sure why drums are on 11 and not 10 like I told them to be
+timidity output.mid -D 11 -Ow -o   - | lame - -b 64 output.mp3
+audacious output.mp3

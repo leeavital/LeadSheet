1ac75e78d81c3427cfa5d6f9f6f20fdb7292e9cf
Lee Avital <leeavital@gmail.com>
2013-10-06 03:04:50 -0400

    Alter build script to use all CLI arguments

--- build_and_play.sh
+++ build_and_play.sh
@@ -1,3 +1,3 @@
 # runs AL mid and plays the file with timidity 
-python al_mid.py infile=examples/autumn_leaves.txt tempo=200
-timidity output.mid -D 11 ## wtf
+python al_mid.py infile=examples/autumn_leaves.txt tempo=200 repeat=2
+timidity output.mid -D 11 ## wtf, should be 11
